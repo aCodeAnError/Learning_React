e98804dea7ddb9b177181d13a6945816e5e6eb5a
aCodeAnError <rk8791137@gmail.com>
2025-02-09 17:41:13 +0530

    learn about tailwind and components

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Card from './components/card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="rahul"/>
+      <Card username="kumar"/>
+    </>
+  )
+}
+
+export default App
